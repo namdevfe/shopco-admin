2e6f0ba6611ee73deb69171347f87054e1d0352e
namdev <namnkq2603@gmail.com>
2025-03-02 19:28:36 +0700

    feat: add permission

--- src/services/permissionService.ts
+++ src/services/permissionService.ts
@@ -1,11 +1,15 @@
 import { ApiResponse } from '~/types/common'
-import { Permission } from '~/types/permission'
+import { AddPermissionPayload, Permission } from '~/types/permission'
 import axiosInstance from '~/utils/axiosInstance'
 
 const permissionService = {
   getAll(): Promise<ApiResponse<Permission[]>> {
     const url = '/permissions/get-all-permissions'
     return axiosInstance.get(url)
+  },
+  add(payload: AddPermissionPayload): Promise<ApiResponse<Permission>> {
+    const url = '/permissions/add-permission'
+    return axiosInstance.post(url, payload)
   }
 }
 
